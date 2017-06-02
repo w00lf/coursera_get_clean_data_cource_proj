3aa526990378091cb66a583086e47f994db5563d
m.taraskin <m.taraskin@rambler-co.ru>
2017-06-02 13:32:38 +0300

    Added some comments to script

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,11 @@
-# Merges the training and the test sets to create one data set.
-# Extracts only the measurements on the mean and standard deviation for each measurement.
-# Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive variable names.
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Script to doenload SAmsung train data and process it to return tidy data
+
+# Assigment requirments, the script will:
+# Merge the training and the test sets to create one data set.
+# Extract only the measurements on the mean and standard deviation for each measurement.
+# Use descriptive activity names to name the activities in the data set
+# Appropriately label the data set with descriptive variable names.
+# Create a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 TRAIN_SET <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
 DESTFILE <- 'train_set_samsung.zip'
@@ -59,7 +62,7 @@ measurments_table <- merge(measurments_table, activities_table, by.X = 'Activity
 
 library(data.table)
 measurments_tbl <- data.table(measurments_table)
-result_measurments_table <- measurments_tbl[,lapply(.SD,mean), by = 'Subject_id,Activity_name', .SDcols = 3:68]
+result_measurments_table <- measurments_tbl[, lapply(.SD, mean), by = 'Subject_id,Activity_name', .SDcols = 3:68]
 
 # Change names of features to appropriate
 colnames(result_measurments_table) <- c(colnames(result_measurments_table)[1:2], sapply(colnames(result_measurments_table)[3:68], function(x) paste('Mean of',x)))
